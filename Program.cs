82e55d105670dfb2f1978aaf5827c9fae044892c
Renato <renato.lage@ligohub.com>
2023-11-10 17:11:47 -0300

    Resolução do desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smarthphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "tijolao", imei:"1234", memoria: 128);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("GitHub");
+
+Console.WriteLine("------------------------------");
+
+Smartphone iphone = new Iphone(numero: "28232393", modelo:"tudo igual", imei:"nemprecisa",memoria:88888);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("PlayStore");
